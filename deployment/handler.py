92b270b13a40c3b93e4dd11bbc6e5b0615a1b28a
Chang-Paris <qi.chang1022@gmail.com>
2022-03-04 10:44:52 +0100

    update with torchserve coding style
    Signed-off-by: Chang-Paris <qi.chang1022@gmail.com>

--- deployment/handler.py
+++ deployment/handler.py
@@ -1,7 +1,40 @@
 import os
-import torch
 import logging
+import cv2
+import torch
+import numpy as np
+
+from collections import OrderedDict
+
 logger = logging.getLogger(__name__)
+
+
+eps=1e-8
+
+def sinkhorn(M,r,c,iteration):
+    p = torch.softmax(M, dim=-1)
+    u = torch.ones_like(r)
+    v = torch.ones_like(c)
+    for _ in range(iteration):
+        u = r / ((p * v.unsqueeze(-2)).sum(-1) + eps)
+        v = c / ((p * u.unsqueeze(-1)).sum(-2) + eps)
+    p = p * u.unsqueeze(-1) * v.unsqueeze(-2)
+    return p
+
+def sink_algorithm(M,dustbin,iteration):
+    M = torch.cat([M, dustbin.expand([M.shape[0], M.shape[1], 1])], dim=-1)
+    M = torch.cat([M, dustbin.expand([M.shape[0], 1, M.shape[2]])], dim=-2)
+    r = torch.ones([M.shape[0], M.shape[1] - 1],device='cuda')
+    r = torch.cat([r, torch.ones([M.shape[0], 1],device='cuda') * M.shape[1]], dim=-1)
+    c = torch.ones([M.shape[0], M.shape[2] - 1],device='cuda')
+    c = torch.cat([c, torch.ones([M.shape[0], 1],device='cuda') * M.shape[2]], dim=-1)
+    p=sinkhorn(M,r,c,iteration)
+    return p
+
+def normalize_size(x,size,scale=1):
+    size=size.reshape([1,2])
+    norm_fac=size.max()
+    return (x-size/2+0.5)/(norm_fac*scale)
 
 
 class SuperGlueHandler:
@@ -14,52 +47,101 @@ class SuperGlueHandler:
         self.config = dict({
             "use_score_encoding": True,
             "layer_num": 9,
-            "sink_iter": [10,100],
+            "sink_iter": [10, 100],
             "head": 4,
             "net_channels": 256
         })  # Model definition configuration => todo clean this
+        self.data = None
 
+    def match_p(self, p):  # p N*M
+        # return the score of matching
+        score, index = torch.topk(p, k=1, dim=-1)  # take max score by row
+        _, index2 = torch.topk(p, k=1, dim=-2)  # take max score by column
+
+        mask_th, index, index2 = score[:, 0] > self.p_th, index[:, 0], index2.squeeze(0)  # filter by score threshold
+
+        mask_mc = index2[index] == torch.arange(len(p)).cuda()
+        mask = mask_th & mask_mc
+        index1, index2 = torch.nonzero(mask).squeeze(1), index[mask]
+        final_score = p[index1, index2]
+        return index1, index2, final_score
+
+    def get_device(self):
+        if torch.cuda.is_available():
+            device = 'cuda:0'
+        else:
+            device = 'cpu'
+        return device
 
     def initialize(self, ctx):
         """
         load eager mode state_dict based model
         """
 
-        properties = ctx.system_properties
-        self.device = torch.device(
-            "cuda:" + str(properties.get("gpu_id"))
-            if torch.cuda.is_available()
-            else "cpu"
-        )
+        self.device = self.get_device()
 
         logger.info(f"Device on initialization is: {self.device}")
-        model_dir = properties.get("model_dir")
+        model_dir = ctx.get("model_dir")
 
-        manifest = ctx.manifest
-        logger.error(manifest)
-        serialized_file = manifest["model"]["serializedFile"]
+        serialized_file = ctx["serializedFile"]
         model_pt_path = os.path.join(model_dir, serialized_file)
         if not os.path.isfile(model_pt_path):
             raise RuntimeError("Missing the model definition file")
 
-        from model import SG_Model
+        from deployment.model import SG_Model
+
         self.model = SG_Model(self.config)
         self.model.cuda(), self.model.eval()
-        state_dict = torch.load(model_pt_path, map_location=self.device)
-        self.model.load_state_dict(state_dict)
+        checkpoint = torch.load(model_pt_path, map_location=self.device)
 
-        # todo load the state dict
+        # load the state dict
+        if list(checkpoint['state_dict'].items())[0][0].split('.')[0] == 'module':
+            new_stat_dict = OrderedDict()
+            for key, value in checkpoint['state_dict'].items():
+                new_stat_dict[key[7:]] = value
+            checkpoint['state_dict'] = new_stat_dict
+        self.model.load_state_dict(checkpoint['state_dict'])
 
     def preprocess(self, data):
-        info_pair = dict()
-        return info_pair
+        # load image
+        img1, img2 = cv2.imread(data["image1_url"]), cv2.imread(data["image2_url"])
+        size1, size2 = np.flip(np.asarray(img1.shape[:2])), np.flip(np.asarray(img2.shape[:2]))
 
-    def inference(self, info_pair):
-        prediction = dict()
+        # load the input kp and descriptors
+        kpt1 = np.load(data["img1_kpt1"])
+        desc1 = np.load(data["img1_desc1"])
+        kpt2 = np.load(data["img1_kpt2"])
+        desc2 = np.load(data["img1_desc2"])
+
+        # compose a new dictionary
+        test_data = {'x1': kpt1, 'x2': kpt2, 'desc1': desc1, 'desc2': desc2, 'size1': size1, 'size2': size2}
+        self.data = test_data
+        return test_data
+
+    def inference(self, data):
+        norm_x1, norm_x2 = normalize_size(data['x1'][:, :2], data['size1']), \
+                           normalize_size(data['x2'][:, :2], data['size2'])
+        x1, x2 = np.concatenate([norm_x1, data['x1'][:, 2, np.newaxis]], axis=-1), np.concatenate(
+            [norm_x2, data['x2'][:, 2, np.newaxis]], axis=-1)
+        feed_data = {'x1': torch.from_numpy(x1[np.newaxis]).cuda().float(),
+                     'x2': torch.from_numpy(x2[np.newaxis]).cuda().float(),
+                     'desc1': torch.from_numpy(data['desc1'][np.newaxis]).cuda().float(),
+                     'desc2': torch.from_numpy(data['desc2'][np.newaxis]).cuda().float()}
+        with torch.no_grad():
+            res = self.model(feed_data, test_mode=True)
+            prediction = res['p']  # matching result matrix => depends on nbr of kp as input
         return prediction
 
     def postprocess(self, prediction):
-        matching_result = dict()
+        index1, index2, scores = self.match_p(prediction[0, :-1, :-1])
+        corr1, corr2 = self.data['x1'][:, :2][index1.cpu()], self.data['x2'][:, :2][index2.cpu()]
+        if len(corr1.shape) == 1:
+            corr1, corr2 = corr1[np.newaxis], corr2[np.newaxis]
+        matching_result = dict({
+            "demo1_pt": corr1,
+            "demo2_pt": corr2,
+            "match_score": scores
+        })
         return matching_result
 
 
@@ -78,3 +160,32 @@ def handle(data, context):
     data = _service.postprocess(data)
 
     return data
+
+"""
+test field
+
+# define data
+data = dict({
+    "image1_url": "../demo/demo_1.jpg",
+    "image2_url": "../demo/demo_1.jpg",
+    "img1_kpt1": "../demo/demo1_kpt.npy",
+    "img1_desc1": "../demo/demo1_desc.npy",
+    "img1_kpt2": "../demo/demo1_kpt.npy",
+    "img1_desc2": "../demo/demo1_desc.npy",
+})
+
+# define contexte
+contexte = dict({
+    "model_dir": "../weights/",
+    "serializedFile": "model_best.pth"
+})
+
+match = handle(data=data, context=contexte)
+print(match)
+
+
+# zip the model
+torch-model-archiver --model-name superglue --version 1.0 --model-file deployment/model.py --serialized-file model_store/model_best.pth --export-path model_store --handler deployment/handler.py -f
+torchserve --start --ncs --model-store model_store --models model_store/superglue.mar
+
+"""
--- deployment/model.py
+++ deployment/model.py
@@ -73,6 +73,7 @@ class attention_block(nn.Module):
 class SG_Model(nn.Module):
     def __init__(self, config):
         nn.Module.__init__(self)
+        config = namedtuple('config', config.keys())(*config.values())
         self.use_score_encoding=config.use_score_encoding
         self.layer_num=config.layer_num
         self.sink_iter=config.sink_iter
@@ -111,60 +112,6 @@ class SG_Model(nn.Module):
         desc_mat = torch.matmul(aug_desc1.transpose(1, 2), aug_desc2)
         p = sink_algorithm(desc_mat, self.dustbin,self.sink_iter[0])
         return {'p':p}
-
-    def __del__(self):
-        if hasattr(self, "sfs"):
-            self.sfs.remove()
-
-
-class GNN_Matcher(object):
-
-    def __init__(self, config):
-        config = namedtuple('config', config.keys())(*config.values())
-        self.p_th = config.p_th
-
-        self.model = SG_Model(config)
-        self.model.cuda(), self.model.eval()
-        checkpoint = torch.load(os.path.join(config.model_dir, 'model_best.pth'))
-        # for ddp model
-        if list(checkpoint['state_dict'].items())[0][0].split('.')[0] == 'module':
-            new_stat_dict = OrderedDict()
-            for key, value in checkpoint['state_dict'].items():
-                new_stat_dict[key[7:]] = value
-            checkpoint['state_dict'] = new_stat_dict
-        self.model.load_state_dict(checkpoint['state_dict'])
-
-
-    def run(self, test_data):
-        norm_x1, norm_x2 = normalize_size(test_data['x1'][:, :2], test_data['size1']), \
-                           normalize_size(test_data['x2'][:, :2], test_data['size2'])
-        x1, x2 = np.concatenate([norm_x1, test_data['x1'][:, 2, np.newaxis]], axis=-1), np.concatenate(
-            [norm_x2, test_data['x2'][:, 2, np.newaxis]], axis=-1)
-        feed_data = {'x1': torch.from_numpy(x1[np.newaxis]).cuda().float(),
-                     'x2': torch.from_numpy(x2[np.newaxis]).cuda().float(),
-                     'desc1': torch.from_numpy(test_data['desc1'][np.newaxis]).cuda().float(),
-                     'desc2': torch.from_numpy(test_data['desc2'][np.newaxis]).cuda().float()}
-        with torch.no_grad():
-            res = self.model(feed_data, test_mode=True)
-            p = res['p']  # matching result matrix => depends on nbr of kp as input
-        index1, index2, scores = self.match_p(p[0, :-1, :-1])
-        corr1, corr2 = test_data['x1'][:, :2][index1.cpu()], test_data['x2'][:, :2][index2.cpu()]
-        if len(corr1.shape) == 1:
-            corr1, corr2 = corr1[np.newaxis], corr2[np.newaxis]
-        return corr1, corr2, scores
-
-    def match_p(self, p):  # p N*M
-        # return the score of matching
-        score, index = torch.topk(p, k=1, dim=-1)  # take max score by row
-        _, index2 = torch.topk(p, k=1, dim=-2)  # take max score by column
-
-        mask_th, index, index2 = score[:, 0] > self.p_th, index[:, 0], index2.squeeze(0)  # filter by score threshold
-
-        mask_mc = index2[index] == torch.arange(len(p)).cuda()
-        mask = mask_th & mask_mc
-        index1, index2 = torch.nonzero(mask).squeeze(1), index[mask]
-        final_score = p[index1, index2]
-        return index1, index2, final_score
 
     def __del__(self):
         if hasattr(self, "sfs"):
